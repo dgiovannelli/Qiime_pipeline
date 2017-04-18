86e1b9da2890ea8b989da713ab023deb6ec562df
Donato Giovannelli <giovannelli@marine.rutgers.edu>
2017-04-18 16:43:47 +0900

    Error correction
    Corrected errors and changed mapping question format

--- qiime_basic_analysis-dev.sh
+++ qiime_basic_analysis-dev.sh
@@ -5,7 +5,7 @@ echo "Starting the QIIME 1.9 pipeline optimized for the polyphemus server. Refer
 
 mkdir results	# prepare the result folder
 print_qiime_config.py -tb > results/analysis_info.txt	# print the qiime config file, script version, analysis date and other basic info for future reference
-echo "Script version v0.3-20170317" >>  results/analysis_info.txt
+echo "Script version v0.4-dev-20170318" >>  results/analysis_info.txt
 date >> results/analysis_info.txt
 echo "Username running the script" $USER >> results/analysis_info.txt
 ls -lah >> results/analysis_info.txt
@@ -13,16 +13,17 @@ echo "Check mapping file"	# Validating the mapping file
 rm -rf results/check_map/ ; validate_mapping_file.py -m map.txt -o results/check_map/	# The rm ensure that previous check attempt in the same folder do not block the script
 echo
 
-read -p "Are there any error in the mapping file? [y/n]" answer
-if [[ $answer = y ]] ; then
+read -p "Do you want to proceed with the analysis? [y/n]" answer
+if [[ $answer = n ]] ; then
 	echo "Check the results/check_map/ folder and correct the map.txt file. Rerun the qiime script"
 	[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but doesn't exit interactive shell
 else
-	echo "No error found in the mapping file. Proceeding with the qiime pipeline"
+	echo "No error found in the mapping file. Proceeding with the qiime DSML basic pipeline"
 	echo
 	echo "Pick open reference OTUs at 97% cut-off using the default GreenGene database"
 	#echo “pick_otus:enable_rev_strand_match True” > results/otu_params.txt		# Enable reverse strand match for paired end reads if not mate paired. Enable if most of the reads fail during this step and add -p results/otu_params.txt  to the below command
 	pick_open_reference_otus.py -o results/otus/ -i seqs.fna -aO 8 # Open reference OTU picking, currently the preferred methods
+	echo
 	echo "Check for chimera sequences and write them to chimeric_otu.txt"
 	identify_chimeric_seqs.py -i results/otus/pynast_aligned_seqs/rep_set_aligned.fasta  -a /usr/local/lib/python2.7/dist-packages/qiime_default_reference/gg_13_8_otus/rep_set_aligned/85_otus.pynast.fasta -o results/chimeric_otu.txt # Check and remove chimera from OTUs selected sequences
 	filter_fasta.py -f results/otus/rep_set.fna -o results/rep_set_cc.fna -s results/chimeric_otu.txt -n	# Remove chimera from OTU table, rep_set fasta and rep_set tree
@@ -42,8 +43,8 @@ else
 	compute_core_microbiome.py --min_fraction_for_core 1 -i results/otu_cc_greengenes.biom -o results/core_microbiome 	# Computing the core biome, aka the OTUs common to all samples. Change the  --min_fraction_for_core parameters if needed [0,1]"
 
 	echo
-	seq_rarefact=$(grep "Min" results/library_summary_cc.txt | cut -d":" -f2)
-	echo "Computing alpha and beta diversity. Rarefaction is set to ${seq_rarefact} sequences based on the Min number of sequences in your library_summary_cc.txt file."
+	seq_rarefact=$(grep "Min" results/library_summary_after_cc.txt | cut -d":" -f2)
+	echo "Computing alpha and beta diversity. Rarefaction is set to ${seq_rarefact} sequences based on the Min number of sequences in your library_summary_after_cc.txt file."
   echo "Diversity rarefaction parameters for alpha and beta diversity set to ${seq_rarefact} sequences" >> results/analysis_info.txt
 	summarize_taxa_through_plots.py -o results/taxa_summary -i results/otu_cc_greengenes.biom -m map.txt		# Making basic summary plots
 	make_otu_network.py -m map.txt -i results/otu_cc_greengenes.biom -o results/network	# Compute and create OTUs network. It can be visualized with Cytoscape or parsed to be used in Circos or HivePlot
